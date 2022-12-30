835e29f9844903fc9c9e0b6eb3d1a282d64736d9
ingrid-oguro <104454935+ingrid-oguro@users.noreply.github.com>
2022-12-30 17:11:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,16 +10,16 @@ st.set_page_config(**PAGE_CONFIG)
 
 mf0 = pd.read_excel('mediafinal.xlsx')
 mf = mf0.query('NOTA <= 10')
-disci = ['Automação Industrial']
-disci2 = mf.query('DISCIPLINA == @disci')
+#disci = ['Automação Industrial']
+#disci2 = mf.query('DISCIPLINA == @disci')
 
 st.subheader('Rendimento na Disciplina')
-disciplina = sorted(disci2.DISCIPLINA.unique())
+disciplina = sorted(mf.DISCIPLINA.unique())
 disciplina_selecionada = st.selectbox('Disciplina',disciplina)
-disci3 = disci2.query('DISCIPLINA == @disciplina_selecionada ')
+disci2 = mf.query('DISCIPLINA == @disciplina_selecionada ')
 
 alt.data_transformers.enable('default', max_rows=None)
-grafico_final = alt.Chart(disci3).mark_circle(size=100).encode(
+grafico_final = alt.Chart(disci2).mark_circle(size=100).encode(
     alt.X('CODPERLET:O',scale=alt.Scale(zero=False) ,axis=alt.Axis( title='Periodo') ),
     alt.Y('NOTA',axis=alt.Axis(title='Nota', orient = "left") ),
     tooltip = ['NOME','RA'],
