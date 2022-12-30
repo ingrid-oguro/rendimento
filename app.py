83b0780deed5897149f0a3c6ec8c8ce9428e537e
ingrid-oguro <104454935+ingrid-oguro@users.noreply.github.com>
2022-12-30 17:07:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-#import plotly.express as px
-import altair as pd
+import altair as alt
 
 import pip
 pip.main(["install", "openpyxl"])
