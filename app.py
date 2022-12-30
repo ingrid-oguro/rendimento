e6443a2cd00b35cd71ff0dce89e8e697af8702c4
ingrid-oguro <104454935+ingrid-oguro@users.noreply.github.com>
2022-12-30 16:54:03 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+#import plotly.express as px
+import altair as pd
+from base import *
+import pip
+pip.main(["install", "openpyxl"])
+from base import *
